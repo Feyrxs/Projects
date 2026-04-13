57781e6e54fed15e5d5c79d7ff76aedff24749b8
Feyrxs <mmarianallimad@gmail.com>
2026-04-13 19:40:41 -0300

    Jogo de pedra papel e tesoura.

---
+++ rock_paper_scissor.py
@@ -0,0 +1,55 @@
+import random
+
+print("Lets play rock paper scissors!")
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+try:
+ user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n").lower())
+ game_image = [rock, paper, scissors]
+ print(game_image[user_choice])
+
+ computer_choice = random.randint(0, 2)
+ if computer_choice == 0:
+     print(f"Computer chose:\n {rock}")
+ elif computer_choice == 1:
+     print(f"Computer chose:\n {paper}")
+ else:
+     print(f"Computer chose: \n {scissors}")
+
+ if user_choice == 0 and computer_choice == 0:
+     print("Its a draw!")
+ elif (user_choice == 0 and computer_choice == 2) or \
+      (user_choice == 1 and computer_choice == 0) or \
+      (user_choice == 2 and computer_choice == 1):
+     print ("You win!")
+ else:
+     print ("You lose!")
+
+except ValueError:
+    print("That's not a number!")
+except IndexError:
+    print("Invalid choice!")
