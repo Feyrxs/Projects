45dbf45912141841be487bcb3665e0bc9f6869e5
Feyrxs <mmarianallimad@gmail.com>
2026-09-25 17:14:33 -0300

    adding and updating functions

--- rock_paper_scissor/rock_paper_scissor.py
+++ rock_paper_scissor/rock_paper_scissor.py
@@ -27,29 +27,38 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-try:
- user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n").lower())
- game_image = [rock, paper, scissors]
- print(game_image[user_choice])
 
- computer_choice = random.randint(0, 2)
- if computer_choice == 0:
-     print(f"Computer chose:\n {rock}")
- elif computer_choice == 1:
-     print(f"Computer chose:\n {paper}")
- else:
-     print(f"Computer chose: \n {scissors}")
+game_image = [rock, paper, scissors]
 
- if user_choice == 0 and computer_choice == 0:
-     print("Its a draw!")
- elif (user_choice == 0 and computer_choice == 2) or \
-      (user_choice == 1 and computer_choice == 0) or \
-      (user_choice == 2 and computer_choice == 1):
-     print ("You win!")
- else:
-     print ("You lose!")
+while True:
+    try:
+        user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n").strip())
 
-except ValueError:
-    print("That's not a number!")
-except IndexError:
-    print("Invalid choice!")
+        print(game_image[user_choice])
+
+        computer_choice = random.randint(0, 2)
+        print(f"Computer chose: {game_image[computer_choice]}")
+
+        if user_choice == computer_choice:
+            print("Its a draw!")
+
+        elif (user_choice == 0 and computer_choice == 2) or \
+            (user_choice == 1 and computer_choice == 0) or \
+            (user_choice == 2 and computer_choice == 1):
+            print ("You win!")
+
+        else:
+            print ("You lose!")
+
+        go_again = input ("Would you like to go again? 'yes' or 'no'\n").strip().lower()
+        if go_again == "yes":
+            continue
+        else:
+            print("Goodbye, friend!")
+            break
+
+    except ValueError:
+        print("That's not a number! Try again.")
+    except IndexError:
+        print("Invalid choice! Try again.")
+
--- treasure_hunt/treasure_hunt.py
+++ treasure_hunt/treasure_hunt.py
@@ -1,59 +1,79 @@
 import art
 import colors
 
-
-def start():
+def show_intro():
     print(colors.yellow + art.chest + colors.reset)
     print("Welcome to Treasure Island.".center(80))
-    print("Your mission is to find the treasure.".center(80))
+    print("Your mission is to find the treasure.\n".center(80))
 
 
-def win(celebration, art):
+def ask_choice(prompt):
+    return input(prompt).strip().lower()
+
+
+def win():
     print(f"""{art.celebration}
                 OOOOoooHuu! {colors.green}You win!{colors.reset}""")
 
 
-left_right = input(f"""You're at a cross road. Where do you want to go?
-        Type "left" or "right"\n """).strip().lower()
+def game_over(message, artwork):
+    print(f"""
+{artwork}
+{message}
+{colors.red}Game over!{colors.reset}
+""")
 
-if left_right == "left":
-    swim_boat = input(f"""You found a lake! 
-        Do you want to "swim" or "wait" for a boat?\n""").strip().lower()
+
+def choose_path():
+    left_right = ask_choice("""You're at a cross road. Where do you want to go?
+Type "left" or "right": """)
+
+    if left_right == "left":
+        choose_lake_action()
+    elif left_right == "right":
+        game_over(
+            "You found Mary Poppins and she's not happy with you!",
+            art.mary_poppins,
+        )
+    else:
+        game_over("You got lost because you chose an invalid path.", art.mary_poppins)
+
+
+def choose_lake_action():
+    swim_boat = ask_choice("""You found a lake!
+Do you want to "swim" or "wait" for a boat? """)
 
     if swim_boat in ("wait", "boat"):
-        which_door = (
-            input(f"""You got to the other side! In front of you there's three doors.
-            {art.door}
-            One {colors.red}red{colors.reset}, one {colors.yellow}yellow{colors.reset}
-            and one {colors.green}green{colors.reset}.
-            Which door do you choose?
-            """).strip().lower()
-        )
+        choose_door()
+    elif swim_boat == "swim":
+        game_over("Attacked by Nessie!", art.nessie)
+    else:
+        game_over("You hesitated for too long.", art.nessie)    
 
-        if which_door == "red":
-            print(f""" {art.cerberus}
-                  Oh no! You found Cerberus.
-                  {colors.red}Game over!{colors.reset}""")
 
-        elif which_door == "yellow":
-            print(f""" {art.celebration}
-                  OOOOoooHuu! {colors.green}You win!{colors.reset}""")
+def choose_door():
+    which_door = ask_choice(
+        f"""You made it to the other side! In front of you there are three doors.
+{art.door}
+One {colors.red}red{colors.reset}, one {colors.yellow}yellow{colors.reset}
+and one {colors.green}green{colors.reset}.
+Which door do you choose? """
+    )
 
-        elif which_door == "green":
-            print(f"""{art.goblin}
-                  Eaten by goblins!
-                  {colors.red}Game over!{colors.reset}""")
+    if which_door == "red":
+        game_over("Oh no! You found Cerberus.", art.cerberus)
 
-        else:
-            print(f"""{art.snail}
-                  {colors.red}Game over!{colors.reset}""")
+    elif which_door == "yellow":
+        win()
+
+    elif which_door == "green":
+        game_over("Eaten by goblins!", art.goblin)
 
     else:
-        print(f"""{art.nessie}
-              Attacked by Nessie!
-              {colors.red}Game over!{colors.reset}""")
+        game_over("You opened a strange door and vanished.", art.snail)
 
-else:
-    print(f"""{art.mary_poppins}
-          You found Mary Poppins and she's not happy with you!
-          {colors.red}Game over!{colors.reset}""")
+
+def main():
+    show_intro()
+    choose_path()
+
